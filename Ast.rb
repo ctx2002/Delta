1b54817a7495fb7f97d1b7a88a94dea29d8e1d89
anru chen <ctx2002@gmail.com>
2011-10-06 21:52:07 +1300

    added more AST classes

--- Ast.rb
+++ Ast.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 module Delta
   class AST
-    attr_accessor(:token,:astName)
+    attr_accessor(:token)
+    attr_accessor(:astName)
     def initialize(token)
       @token = token
     end
@@ -18,8 +19,65 @@ module Delta
   class IdentifierAST < AST
     def initialize(token)
       super(token)
-      @astName = "identifierAST"
+      #@astName = "identifierAST"
     end
   end
   
+  class ConstDeclaration < AST
+    attr_accessor(:identifierAST,:expressionAST)  
+    def initialize(identifier,expression,token)
+        super(token)
+        @identifierAST = identifierAST
+        @expressionAST = expression
+      end  
+  end
+  
+  class AnyTypeDenoter < TypedenoterAST 
+  end
+  
+  class ExpressionAST < AST 
+    
+  end
+  
+  class VnameAST < AST
+  end
+  class CommandAST < AST 
+  end
+  class DeclarationAST < AST
+  end
+  class TypedenoterAST < AST
+  end
+  class ActualParameterAST < AST
+  end
+  class ActualParameterSequenceAST < AST
+  end
+  
+  class ArrayAggregateAST < AST
+    attr_accessor(:eleCount)   
+  end
+  class ArrayExpression < ExpressionAST
+    attr_accessor(:AA)
+    def initialize(arrayAggregateAST ,token)
+      super(token)
+      @AA = arrayAggregateAST
+    end
+  end
+  
+  class ArrayTypeDenoter < TypedenoterAST
+    attr_accessor(:IL,:T)
+    def initialize(integerLiteralAST, typeDenoterAST,token)
+      super(token)
+      @IL = integerLiteralAST
+      @T = typeDenoterAST
+    end
+  end
+  
+  class AssignCommandAST < CommandAST
+    attr_accessor(:V,:E)
+    def initialize(vnameAST,expressionAST,token)
+      super(token)
+      @V = vnameAST
+      @E = expressionAST
+    end
+  end
 end
